4d5f6a2c005a0c733f8aeabac32c68258eafbcdf
谦勤 <qianqin.zl@alibaba-inc.com>
2021-04-15 18:28:15 +0800

    update

--- zip.js
+++ zip.js
@@ -6,7 +6,7 @@ program.option('-b, --branch <string>', 'Build branch')
   .option('-m, --message <string>', 'Commit Message');
 program.parse(process.argv);
 const options = program.opts();
-console.log('1111112s', options);
+console.log('111112s', options);
 
 //1. 把代码打包 zip
 console.log(process.cwd())
